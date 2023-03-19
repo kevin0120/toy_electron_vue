70834164bfb3784252557a53f5580d34dab1892a
kevin0120 <jingwei1224@163.com>
2023-03-19 20:40:15 +0800

    pretter

--- http/http-server.js
+++ http/http-server.js
@@ -9,17 +9,27 @@ const app = express();
 const server = http.createServer(app);
 
 // 创建 WebSocket 服务器
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({server});
+
+
+app.use((req, res, next) =>{
+    // 设置 CORS 响应头
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next()
+})
 
 // 处理 GET /hello 请求
-app.get('/hello', (req, res) => {
+app.get('/api/hello', (req, res) => {
     res.send('Hello, World!');
 });
 
 // 监听 WebSocket 连接事件
 wss.on('connection', (ws) => {
     console.log('WebSocket connected');
-
+    now = new Date().toLocaleTimeString()
+    ws.send(`hello websocket ${now}`)
     // 监听 WebSocket 消息事件
     ws.on('message', (data) => {
         console.log(`Received message: ${data}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,100 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {VueLoaderPlugin} = require("vue-loader");
 
 module.exports = {
     mode: 'development',
-    entry: "./test_webpack/entry/index.js",
-    output: {
-        path: path.join(__dirname, 'test_webpack/dist'),
-        filename: "main.js"
-    },
+    // entry: "./test_webpack/entry/index.js",
+    // output: {
+    //     path: path.join(__dirname, 'test_webpack/dist'),
+    //     filename: "main.js"
+    // },
     module: {
         rules: [
             {
                 test: /\.ts$/,
                 exclude: /node_modules[^(react\-pdf)]/,
                 use: {
-                    loader: 'babel-loader',
+                    loader: "babel-loader",
                     options: {
-                        cacheDirectory: true
-                    }
-                }
-            }
+                        cacheDirectory: true,
+                    },
+                },
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        cacheDirectory: true,
+                    },
+                },
+            },
 
-        ]
-    }
+            {
+                test: /.vue$/,
+                loader: "vue-loader",
+            },
+
+            {
+                test: /.svg$/,
+                use: {
+                    loader: "svg-sprite-loader",
+                    options: {
+                        symbolId: 'icon-[name]',
+                    },
+
+                },
+            },
+
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+
+            // webpack5已经升级
+            {
+                test: /\.(png|jpg|gif)$/,
+                type: "asset/resource",
+            },
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./test_webpack/entry/index.html", // 以当前文件为模板创建新的HtML(1. 结构和原来一样 2. 会自动引入打包的资源)
+        }),
+        new webpack.EnvironmentPlugin({
+            NODE_ENV: "development", // use 'development' unless process.env.NODE_ENV is defined
+            DEBUG: true,
+        }),
+
+        // 与vue-loader结合使用
+        new VueLoaderPlugin(),
+    ],
+    devServer: {
+        proxy: {
+            "/api": {
+                target: "http://211.254.254.251:3000",
+                pathRewrite: { "^/api": "" },
+            },
+            "/websocket": {
+                target: "ws://211.254.254.251:9001/", //代理的后台地址
+                ws: true,
+                changOrigin: false,
+                pathRewrite: {
+                    "^/websocket": "",
+                },
+            },
+        },
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        // contentBase: resolve(__dirname, 'dist'), // 运行项目的目录
+        open: true, // 自动打开浏览器
+        compress: true, // 启动gzip压缩
+        port: "auto", // 端口号
+        hot: true, // 开启热模替换功能 HMR
+    },
 };
