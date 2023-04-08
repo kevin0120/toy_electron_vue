07e8d7a4eb167c6c4d44782ace7e549a31aa754d
kevin0120 <jingwei1224@163.com>
2023-04-08 18:16:44 +0800

    dd

--- app/vue_app/src/view/svgimage/components/HelloSvg.vue
+++ app/vue_app/src/view/svgimage/components/HelloSvg.vue
@@ -1,8 +1,8 @@
 <template>
   <div style="width: 10%">
-    <svg-loader class="alarm-icon" :icon="icon" size="3rem"></svg-loader>
+    <svg-loader class="alarm-icon" :icon="icon" size="3rem" :color="color" :stroke="stroke"></svg-loader>
     <br>
-    <span>{{icon}}</span>
+    <span>{{ icon }}</span>
   </div>
 </template>
 
@@ -15,9 +15,27 @@ export default {
   },
   data() {
     return {
+      color: "red",
+      stroke: "green"
     }
   },
+  methods: {
 
+    getRandomColor() {
+      var letters = "0123456789ABCDEF";
+      var color = "#"
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+      }
+      return color
+    }
+  },
+  created() {
+    setInterval(() => {
+    this.color=this.getRandomColor();
+    this.stroke=this.getRandomColor();
+    }, 500)
+  },
 
 }
 </script>
@@ -27,14 +45,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
